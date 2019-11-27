5e8a910456b0b256d5c7723e7881a61baf86cbee
lorelei47 <948866419@qq.com>
2019-11-27 18:09:00 +0800

    Array,Date,RegExp

--- read/node/main.js
+++ read/node/main.js
@@ -1,26 +1,5 @@
 //此js文件做常规运行作用，可随时更换代码
 
-function taskA(){
-  console.log("这是A");
-}
-function taskB(){
-  var result = setTimeout(function(){
-    console.log('异步任务的结果B')
-  }, 3000)
-  return result
-}
-function taskC(){
-  console.log("这是C");
-}
-taskA();
-taskB();
-taskC();
-//执行结果
-//--------
-//这是A
-//这是C
-//异步任务的结果B
+var date = new Date("2017-1-1");
 
-/*******************/
-
-/*******************/
+console.log(date);
