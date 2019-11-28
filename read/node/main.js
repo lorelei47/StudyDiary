5e6c1948240dc13e98764296458332a4cdd7ec19
lorelei47 <948866419@qq.com>
2019-11-28 11:05:04 +0800

    单体内置对象

--- read/node/main.js
+++ read/node/main.js
@@ -1,6 +1,5 @@
 //此js文件做常规运行作用，可随时更换代码
 
-var s1 = "jhhhhh"
-var s2 = s1.substring(2)
-console.log(typeof s1)
-console.log(typeof s2)
+var s1 = Math.floor(Math.random() * 100 + 1);
+
+console.log(s1)
